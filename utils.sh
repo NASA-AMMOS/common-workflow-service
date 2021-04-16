6571eb8d78666dc59ee8fa304da78be3d2486c5c
James Wood <James.F.Wood@jpl.nasa.gov>
2021-04-16 10:40:27 -0700

    Update versions

--- utils.sh
+++ utils.sh
@@ -5,7 +5,7 @@
 # Shell utility functions and environment settings used throughout CWS setup scripts
 
 # Update versions as necessary
-export CWS_VER='2.0'  # update this each CWS release
+export CWS_VER='2.1.0-pre.3'  # update this each CWS release
 export CAMUNDA_VER='7.13.0'
 export TOMCAT_VER='9.0.33'
 export LOGSTASH_VER='7.9.0'
