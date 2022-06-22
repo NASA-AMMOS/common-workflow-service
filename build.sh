72193711a8774889c799b9baef7cc135c3a4735b
RonnyFray <ronnyfray@gmail.com>
2022-06-22 09:33:29 -0700

    Updated yml file.

--- build.sh
+++ build.sh
@@ -31,6 +31,7 @@ mvn -DskipTests -Dskip.integration.tests clean install -P ${BUILD_PROFILE}
 if [[ $? -eq 0 ]]; then
     # Build distribution
     print "Creating server distribution..."
+    echo pwd + " directory for build.sh"
     ${ROOT}/create_server_dist.sh
 else
     print "ERROR: There were one or more build errors, aborting."
