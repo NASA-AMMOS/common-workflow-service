f12cbc219ca9b2507a3e8a9fcbdbb85f6391f7e5
zonagit <zef.andrade@gmail.com>
2022-08-05 15:01:16 -0700

    Add ability to invoke adaptation js code to the workers page (#113)
    Co-authored-by: zef <zandrade@jpl.nasa.gov>

---
+++ cws-ui/src/main/webapp/js/adaptation-workers.js
@@ -0,0 +1,6 @@
+// override with js code file in your adaptation project for example like this
+// cp your-cws-adaptation/src/main/resources/cws-ui/adaptation-workers.js common-workflow-service/cws-ui/src/web/main/webapp/js/adaptation-workers.js
+function adaptationWorkersReady() {
+	return; // CWS core behavior is to do nothing here
+}
+
