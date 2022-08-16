43b0d581cf99aa70ac2d73fe539fabb7ffbf52e6
RonnyFray <ronnyfray@gmail.com>
2022-08-16 14:19:24 -0700

    Test sending workflow failure message. Add webhook payload variable.

--- cws-test/src/test/java/jpl/cws/test/WebTest.java
+++ cws-test/src/test/java/jpl/cws/test/WebTest.java
@@ -38,7 +38,7 @@ public class WebTest extends WebTestUtil {
 		log.info("------ START runProcessTest ------");
 		gotoLoginPage();
 		login();
-		startProcDef("test_simplest", "Test Simplest", 90000);
+		startProcDef("tes", "Test Simplest", 90000); // simulate unit test failure
 		deleteProc("test_simplest");
 		logout();
 		log.info("------ END runProcessTest ------");
