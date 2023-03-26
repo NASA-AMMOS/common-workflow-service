e2808bafc1cea6b49ef78a4f119cd4f34c63aad0
Ronny Fray <ronnyfray@gmail.com>
2023-03-26 16:33:04 -0400

    Update Camunda to v7.18 Remove cws_camunda-bpm-tomcat-7.17.0.zip Remove cws_camunda-bpm-tomcat-7.17.0-lib.zip Add cws_camunda-bpm-tomcat-7.18.0.zip Add cws_camunda-bpm-tomcat-7.18.0-lib.zip Refactor for Camunda 7.18 Set Content Security Policy header value

--- utils.sh
+++ utils.sh
@@ -6,7 +6,7 @@
 
 # Update versions as necessary
 export CWS_VER='2.4.0pre.1'    # update this each CWS release
-export CAMUNDA_VER='7.17.0'
+export CAMUNDA_VER='7.18.0'
 export TOMCAT_VER='9.0.58'
 export LOGSTASH_VER='7.16.2'
 
