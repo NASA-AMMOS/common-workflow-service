17acfbd0edac4e7f8faac7e3fc27a7473b948954
Will Gunter <wgunter@udel.edu>
2023-07-26 18:55:39 -0700

    Attempt 2

--- cws-service/src/main/java/jpl/cws/controller/RestService.java
+++ cws-service/src/main/java/jpl/cws/controller/RestService.java
@@ -688,8 +688,14 @@ public class RestService extends MvcCore {
 
 		try {
 			//we need to decode source
-			String result = source.replaceAll("%2B", "+");
-			result = java.net.URLDecoder.decode(source, StandardCharsets.UTF_8.name());
+			log.debug("GET NO SCROLL BEFORE REPLACEMENT: " + source);
+			String result = java.net.URLDecoder.decode(source, StandardCharsets.UTF_8.name());
+			
+			//get index of 00:00
+			int index = result.indexOf("00:00");
+
+			//replace space in front of 00:00 with +
+			result = result.substring(0, index) + "+" + result.substring(index + 1);
 
 			log.debug("logs/get/noScroll: result: " + result);
 			RestCallResult restCallResult;
