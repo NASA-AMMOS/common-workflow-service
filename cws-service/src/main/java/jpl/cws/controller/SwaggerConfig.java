d3af1ae30a88e789a8cdfc832d7547c01d222fb7
wcgunter <wgunter@udel.edu>
2024-03-19 17:39:23 -0400

    Add basic swagger documentation (when you run, it's available at localhost:38443/cws-ui/swagger-ui/index.html)

---
+++ cws-service/src/main/java/jpl/cws/controller/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package jpl.cws.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("API Desc")
+                .version("V1")
+                .build();
+    }
+
+    @Bean
+    public UiConfiguration uiConfiguration() {
+        return UiConfigurationBuilder
+                .builder()
+                .defaultModelExpandDepth(-1)
+                .build();
+    }
+}
