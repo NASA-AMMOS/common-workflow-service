18c1a7c986de231297ed41a39bf225479e66d648
Jason Han <jason.han@jpl.nasa.gov>
2025-07-16 21:36:48 -0700

    Add missing import

--- cws-core/src/main/java/jpl/cws/core/db/SchedulerDbService.java
+++ cws-core/src/main/java/jpl/cws/core/db/SchedulerDbService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DeadlockLoserDataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
